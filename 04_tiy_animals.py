142a23c435366bc55b005ffff72c95640d44512e
Ktulu1 <ktulu_1@yahoo.com>
2022-01-12 15:54:55 -0500

    Looping lists

---
+++ 04_tiy_animals.py
@@ -0,0 +1,8 @@
+animals = ['squirrels', 'rabbits', 'deer']
+
+for animal in animals:
+    print("\n")
+    print(animal.title())
+    print(f"{animal} are delicious.\n")
+
+print("\nAll these animals are delicious!\n")
---
+++ 04_tiy_pizzas.py
@@ -0,0 +1,7 @@
+pizzas = ['deep dish', 'flat bread', 'hand tossed', 'wood oven']
+for pizza in pizzas:
+    print("\n")
+    print(pizza.title())
+    print(f"I like {pizza}.")
+
+print("\nI like pizza!\n")
