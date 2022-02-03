9f1052b9ec7e606209e9e8577a8afaa24e8a179a
Ktulu1 <ktulu_1@yahoo.com>
2022-02-03 15:39:30 -0500

    Slicing Lists

---
+++ 04_tiy_our pizzas.py
@@ -0,0 +1,11 @@
+pizzas = ['deep dish', 'flat bread', 'hand tossed', 'wood oven']
+friend_pizzas = pizzas[:]
+
+pizzas.append('chicago')
+friend_pizzas.append('hawaiian')
+
+print("My favorite pizzas are:")
+print(pizzas)
+
+print("\nMy friends favorite pizzas are:")
+print(friend_pizzas)
---
+++ 04_tiy_slices.py
@@ -0,0 +1,8 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(f"The first three items in the list are: {players[:3]}.\n")
+print(f"Three items from the middle of the list are: {players[1:4]}.\n")
+print(f"The last three items in the list are: {players[2:]}.\n")
+
+
+
+
