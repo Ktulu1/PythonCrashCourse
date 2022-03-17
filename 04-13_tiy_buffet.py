026b217fbb9e1c369dd79f7383defd8343e9b88c
Ktulu1 <ktulu_1@yahoo.com>
2022-03-17 15:08:25 -0400

    tuples

---
+++ 04-13_tiy_buffet.py
@@ -0,0 +1,11 @@
+foods = ('hotdog', 'hamburger', 'pizza', 'taco', 'chili')
+print("\nOriginal Menu:")
+for food in foods:
+    print(food)
+
+foods = ('fries', 'hamburger', 'pizza', 'onion rings', 'chili')
+print("\nNew Menu:")
+for food in foods:
+    print(food)
+
+
